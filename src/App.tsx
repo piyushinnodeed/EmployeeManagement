de7c9c4a7b0e3c00e0784c5e265d223df55d6081
piyushinnodeed <piyush.deshmukh@innodeed.com>
2022-01-12 23:08:05 +0530

    initaial commit - navigation added

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
+import Error from './pages/Error';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Routes>
+      <Route path="/dashboard" element={<Dashboard/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+      <Route path="*" element={<Error/>}/>
+    </Routes>
+    </>
   );
 }
 
---
+++ src/pages/Profile.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Profile() {
+  return (
+    <>
+    My Profile
+    </>
+  );
+}
+
+export default Profile;
